fd0a1f46406367d7c3d0fbda2293c53266fa11d8
Sandipan Adhikary <87486032+Pied07@users.noreply.github.com>
2024-09-30 15:56:08 +0530

    Update views.py

--- profile_app/views.py
+++ profile_app/views.py
@@ -8,7 +8,7 @@ def home(request):
 
 def download_resume(request):
     try:
-        url = f"https://res.cloudinary.com/dbgvwyhdr/raw/upload/v1727232856/uhyh17rllmgo6z64wiff.pdf"
+        url = f"https://res.cloudinary.com/dbgvwyhdr/raw/upload/v1727691791/zy6006x2jlcodzc0kffs.pdf"
         response = HttpResponse(content_type='application/pdf')
         response['Content-Disposition'] = f'attachment; filename="Sandipan_Adhikary_Resume.pdf"'
         response.write(requests.get(url).content)
